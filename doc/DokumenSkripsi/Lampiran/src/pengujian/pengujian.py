c04fbf18f86531c23f48d26c4b1e6877c37088d2
Chris Eldon <eldon922@gmail.com>
2020-05-16 18:16:49 +0700

    Perhitungan manual

--- doc/DokumenSkripsi/Lampiran/src/pengujian/pengujian.py
+++ doc/DokumenSkripsi/Lampiran/src/pengujian/pengujian.py
@@ -21,17 +21,38 @@ df_projected = pd.read_csv('projected_train.csv', delimiter = ',')
 asli = df_asli.drop(['subject', 'Activity'], axis = 1).values
 projected = df_projected.drop(['subject', 'Activity'], axis = 1).values
 
-result = []
 eps = 0.52
 for i in range(0, asli.shape[0]):
     for j in range(i, asli.shape[0]):
         d_asli = distance.euclidean(asli[i], asli[j])
         d_projected = distance.euclidean(projected[i], projected[j])
         if (1-eps) * d_asli**2 > d_projected**2 or d_projected**2 > (1+eps) * d_asli**2:
-            print("------------ERROR: " + str(i) + " " + str(j) + " ------------------")
+            print("------------ERROR: " + str(i+1) + " " + str(j+1) + ": " + 
+                  str(d_asli) + " " + str(d_projected) + " ------------------")
             sys.exit()
         else:
-            print(str(i) + " " + str(j) + ": " + str(d_asli) + " " + str(d_projected))
+            print(str(i+1) + " " + str(j+1) + ": " + str(d_asli) + " " + str(d_projected))
             
 print("ok")
+
+
+
+# Berikut adalah kode untuk menguji hasil Random Rotation Perturbation
+# apakah mempunyai jarak Euclidean yang sama persis dengan aslinya
+df_asli = pd.read_csv('Mall_Customers.csv', delimiter = ',')
+asli = df_asli[['Age' , 'Annual Income (k$)' ,'Spending Score (1-100)']].values
+df_rotated = pd.read_csv('rotated_Mall_Customers.csv', delimiter = ',')
+rotated = df_rotated[['Age' , 'Annual Income (k$)' ,'Spending Score (1-100)']].values
+
+for i in range(0, asli.shape[0]):
+    for j in range(i, asli.shape[0]):
+        d_asli = round(distance.euclidean(asli[i], asli[j]), 2)
+        d_rotated = round(distance.euclidean(rotated[i], rotated[j]), 2)
+        if d_asli != d_rotated:
+            print("------------ERROR: " + str(i+1) + " " + str(j+1) + ": " + 
+                  str(d_asli) + " " + str(d_rotated) + " ------------------")
+            sys.exit()
+        else:
+            print(str(i+1) + " " + str(j+1) + ": " + str(d_asli) + " " + str(d_rotated))
             
+print("ok")
